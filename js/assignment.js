7313735762ec2e38655cabe70ea10f2af666fe6b
ovovoyun <136236394+ovovoyun@users.noreply.github.com>
2023-08-09 20:54:02 +0800

    add assignment.html

---
+++ js/assignment.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+    /*---- table data -----*/
+    const data = [
+        { subTitle: "Chapter Assignment 2", time: "4", title: "Please check this file…", name: "Emily Wang" },
+        { subTitle: "Chapter Assignment 1", time: "12", title: "My Homework", name: "David Xu" },
+        { subTitle: "Final Assignment", time: "18", title: "Have a problem with this…", name: "Tony Wei" },
+        { subTitle: "Final Assignment", time: "19", title: "Did I do it right?", name: "Christina Lin" },
+    ];
+    
+    const listItems = data.map(item => {
+        const activeClass = (item === data[0]) ? "active" : "";
+        
+        return `
+            <li class="list-group-item p-12 ${activeClass}">
+                <div class="d-flex justify-content-between mb-1">
+                    <h5 class="fs-12 mb-0">${item.subTitle}</h5>
+                    <span class="fs-12">${item.time} hrs ago</span>
+                </div>
+                <div class="d-flex justify-content-between align-items-center">
+                    <h4 class="fs-6 fw-bold mb-0">${item.title}</h4>
+                    <span class="fs-12 text-gray600">${item.name}</span>
+                </div>
+            </li>
+        `;
+    });
+    
+    const list = document.getElementById("assignment-list");
+    list.innerHTML = listItems.join("");
+});
--- js/main.js
+++ js/main.js
@@ -4,4 +4,3 @@ import * as bootstrap from 'bootstrap';
 import './jquery-3.7.0.min';
 import 'datatables.net';
 import 'datatables.net-bs5';
-import './index';
