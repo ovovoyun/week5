170a0e1e8fd81c1c7afce1127b1a016714c3ceee
ovovoyun <136236394+ovovoyun@users.noreply.github.com>
2023-08-09 21:51:36 +0800

    clean code

--- js/main.js
+++ js/main.js
@@ -2,6 +2,5 @@ import '../assets/scss/all.scss';
 
 import * as bootstrap from 'bootstrap';
 import './jquery-3.7.0.min';
-import 'datatables.net';
-import 'datatables.net-bs5';
-import './ckeditor';
+// import 'datatables.net';
+// import 'datatables.net-bs5';
