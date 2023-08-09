540bcb5a2a37a4803c761a6caae38cfc0ebd9719
ovovoyun <136236394+ovovoyun@users.noreply.github.com>
2023-08-09 21:48:06 +0800

    Updates

--- assets/assignmentEdit-f59e43d1.js
+++ assets/assignmentEdit-f59e43d1.js
@@ -1,4 +1,4 @@
-import"./dataTables.bootstrap5-2a2d4a4a.js";document.addEventListener("DOMContentLoaded",function(){const e=[{subTitle:"Chapter Assignment 2",time:"4",title:"Please check this file…",name:"Emily Wang"},{subTitle:"Chapter Assignment 1",time:"12",title:"My Homework",name:"David Xu"},{subTitle:"Final Assignment",time:"18",title:"Have a problem with this…",name:"Tony Wei"},{subTitle:"Final Assignment",time:"19",title:"Did I do it right?",name:"Christina Lin"}],s=e.map(t=>`
+import"./ckeditor-34aa5aca.js";document.addEventListener("DOMContentLoaded",function(){const e=[{subTitle:"Chapter Assignment 2",time:"4",title:"Please check this file…",name:"Emily Wang"},{subTitle:"Chapter Assignment 1",time:"12",title:"My Homework",name:"David Xu"},{subTitle:"Final Assignment",time:"18",title:"Have a problem with this…",name:"Tony Wei"},{subTitle:"Final Assignment",time:"19",title:"Did I do it right?",name:"Christina Lin"}],s=e.map(t=>`
             <li class="list-group-item p-12 ${t===e[0]?"active":""}">
                 <div class="d-flex justify-content-between mb-1">
                     <h5 class="fs-12 mb-0">${t.subTitle}</h5>
