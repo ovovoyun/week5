db939c5fe0a5ab310cc82daac851a09518719d7f
ovovoyun <136236394+ovovoyun@users.noreply.github.com>
2023-08-11 12:23:29 +0800

    Updates

---
+++ assets/admin-4b4d694b.js
@@ -0,0 +1,10 @@
+import"./jquery-3.7.0.min-786073e1.js";document.addEventListener("DOMContentLoaded",function(){const a=[{id:1,name:"Emir Wicks",email:"emir.wicks@mail.com",verified:"Yes"},{id:2,name:"Zaina Goldsmith",email:"zaina.goldsmith@mail.com",verified:"Yes"},{id:3,name:"Mahima Lopez",email:"mahima.lopez@mail.com",verified:"Yes"},{id:4,name:"Pharrell Murray",email:"pharrell.murray@mail.com",verified:"Yes"},{id:5,name:"Annika Mcbride",email:"annika.mcbride@mail.com",verified:"Yes"},{id:6,name:"Fatimah Clark",email:"fatimah.clark@mail.com",verified:"Yes"},{id:7,name:"Klaudia Rhodes",email:"klaudia.rhodes@mail.com",verified:"Yes"},{id:8,name:"Tillie Lucero",email:"tillie.lucero@mail.com",verified:"Yes"},{id:9,name:"Sabrina Stephenson",email:"sabrina.stephenson@mail.com",verified:"Yes"},{id:10,name:"Annie Smith",email:"annie.smith@mail.com",verified:"Yes"}],t=document.getElementById("tbody");a.forEach(e=>{const i=document.createElement("tr");i.innerHTML=`
+            <td>#${e.id}</td>
+            <td>${e.name}</td>
+            <td>${e.email}</td>
+            <td>${e.verified}</td>
+            <td>
+                <span class="material-icons me-4 pointer" data-bs-toggle="modal" data-bs-target="#viewModal">visibility</span>
+                <span class="material-icons pointer" data-bs-toggle="modal" data-bs-target="#editModal">edit</span>
+            </td>
+        `,t.appendChild(i)});const d=document.getElementById("table__idSort"),n=document.getElementById("table__downIcon"),m=document.getElementById("table__upIcon");d.addEventListener("click",function(){m.classList.toggle("d-none"),n.classList.toggle("d-none")})});
--- assets/assignmentEdit-ee3c4e83.js
+++ assets/assignmentEdit-ee3c4e83.js
@@ -1,4 +1,4 @@
-import"./jquery-3.7.0.min-650a0682.js";document.addEventListener("DOMContentLoaded",function(){const e=[{subTitle:"Chapter Assignment 2",time:"4",title:"Please check this file…",name:"Emily Wang"},{subTitle:"Chapter Assignment 1",time:"12",title:"My Homework",name:"David Xu"},{subTitle:"Final Assignment",time:"18",title:"Have a problem with this…",name:"Tony Wei"},{subTitle:"Final Assignment",time:"19",title:"Did I do it right?",name:"Christina Lin"}],s=e.map(t=>`
+import"./jquery-3.7.0.min-786073e1.js";document.addEventListener("DOMContentLoaded",function(){const e=[{subTitle:"Chapter Assignment 2",time:"4",title:"Please check this file…",name:"Emily Wang"},{subTitle:"Chapter Assignment 1",time:"12",title:"My Homework",name:"David Xu"},{subTitle:"Final Assignment",time:"18",title:"Have a problem with this…",name:"Tony Wei"},{subTitle:"Final Assignment",time:"19",title:"Did I do it right?",name:"Christina Lin"}],s=e.map(t=>`
             <li class="list-group-item p-12 ${t===e[0]?"active":""}">
                 <div class="d-flex justify-content-between mb-1">
                     <h5 class="fs-12 mb-0">${t.subTitle}</h5>
